51e4a7449633bd70651355af6448f1df0fa454df
sandeep <sandeepprabhakula10@gmail.com>
2022-07-15 08:43:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var _CONTENT = [
+    "Hi,I'm Sandeep",
+    "Engineer",
+    "Android App Developer"
+];
+var _PART = 0;
+var _PART_INDEX = 0;
+var _INTERVAL_VAL;
+var _ELEMENT = document.querySelector("#text");
+var _CURSOR = document.querySelector("#cursor");
+function Type() {
+    var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+    _ELEMENT.innerHTML = text;
+    _PART_INDEX++;
+    if (text === _CONTENT[_PART]) {
+        _CURSOR.style.display = 'none';
+
+        clearInterval(_INTERVAL_VAL);
+        setTimeout(function () {
+            _INTERVAL_VAL = setInterval(Delete, 50);
+        }, 1000);
+    }
+}
+
+function Delete() {
+    var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+    _ELEMENT.innerHTML = text;
+    _PART_INDEX--;
+    if (text === '') {
+        clearInterval(_INTERVAL_VAL);
+        if (_PART == (_CONTENT.length - 1))
+            _PART = 0;
+        else
+            _PART++;
+
+        _PART_INDEX = 0;
+        setTimeout(function () {
+            _CURSOR.style.display = 'inline-block';
+            _INTERVAL_VAL = setInterval(Type, 100);
+        }, 200);
+    }
+}
+_INTERVAL_VAL = setInterval(Type, 100);
+
+// popup logic 
+const popup = document.querySelector('.full-screen');
+const shortbar = document.querySelector('.short-bar')
+setTimeout(showPopup, 3000);
+function showPopup() {
+    popup.classList.remove('hidden');
+    shortbar.classList.add('hidden')
+}
+function closePopup() {
+    popup.classList.add('hidden');
+    shortbar.classList.remove('hidden')
+}
