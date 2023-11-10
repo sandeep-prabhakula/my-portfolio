73150093903d101270be1aa5e41df8d7364d4db5
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2023-11-10 23:24:59 +0530

    enhanced typewriter animation

--- app.js
+++ app.js
@@ -1,46 +1,22 @@
-var _CONTENT = [
-    "Engineer",
-    "Android App Developer",
-    "Backend Developer"
-];
-var _PART = 0;
-var _PART_INDEX = 0;
-var _INTERVAL_VAL;
-var _ELEMENT = document.querySelector("#text");
-var _CURSOR = document.querySelector("#cursor");
-function Type() {
-    var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
-    _ELEMENT.innerHTML = text;
-    _PART_INDEX++;
-    if (text === _CONTENT[_PART]) {
-        _CURSOR.style.display = 'none';
+//typewriter
+var app = document.getElementById('text');
 
-        clearInterval(_INTERVAL_VAL);
-        setTimeout(function () {
-            _INTERVAL_VAL = setInterval(Delete, 50);
-        }, 20);
-    }
-}
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
 
-function Delete() {
-    var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
-    _ELEMENT.innerHTML = text;
-    _PART_INDEX--;
-    if (text === '') {
-        clearInterval(_INTERVAL_VAL);
-        if (_PART == (_CONTENT.length - 1))
-            _PART = 0;
-        else
-            _PART++;
-
-        _PART_INDEX = 0;
-        setTimeout(function () {
-            _CURSOR.style.display = 'inline-block';
-            _INTERVAL_VAL = setInterval(Type, 100);
-        }, 200);
-    }
-}
-_INTERVAL_VAL = setInterval(Type, 100);
+typewriter
+  .pauseFor(100)
+  .typeString('Engineer')
+  .pauseFor(300)
+  .deleteChars(10)
+  .typeString('Android App Developer')
+  .pauseFor(300)
+  .deleteChars(22)
+  .typeString('Backend Developer')
+  .pauseFor(100)
+  .start();
 
 // Navbar toggeling logic
 document.querySelectorAll('.nav-item').forEach(function (element) {
